0f91f1aea8d26d21b66e0e5b13754ddf4c1c041c
Panto! <guustavopantoja@gmail.com>
2023-04-14 20:59:00 -0300

    feat: cache on roadmap route

--- pages/api/roadmap.ts
+++ pages/api/roadmap.ts
@@ -5,6 +5,7 @@ export default async function handler(req: NextApiRequest,res: NextApiResponse) 
 
     try {
         const roadmap = await getRoadmap();
+        res.setHeader('Cache-Control', 's-maxage=86400');
         res.json(roadmap);
     } catch (e) {
         console.error(e);
