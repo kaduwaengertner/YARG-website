a4e173b0f4a2f67660fcd03f70a24e53a2a80e09
Panto! <guustavopantoja@gmail.com>
2023-04-15 00:18:46 -0300

    chore: add fontawesome icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,9 @@ import type { AppProps } from 'next/app'
 import { Barlow } from 'next/font/google';
 
 const barlow = Barlow({ weight: ['600', '700'], subsets: ["latin"] });
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
 
 export default function App({ Component, pageProps }: AppProps) {
   return (<>
