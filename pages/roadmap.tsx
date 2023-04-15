37e29d0001c117d5190e6d5a0eef383f7cca97fc
Panto! <guustavopantoja@gmail.com>
2023-04-14 23:47:02 -0300

    feat: initial roadmap

---
+++ pages/roadmap.tsx
@@ -0,0 +1,16 @@
+import Footer from '@/components/Footer'
+import MenuHeader from '@/components/MenuHeader'
+import Head from 'next/head'
+
+export default function Roadmap() {
+    return (<>
+        <Head>
+            <title>Roadmap - Yet Another Rhythm Game</title>
+        </Head>
+
+        <MenuHeader />
+
+        <Footer />
+        </>
+    );
+}
