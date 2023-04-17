2b64580a7505ae1e6f62b2d6829c83de633626e3
Panto! <guustavopantoja@gmail.com>
2023-04-17 04:55:13 -0300

    chore(getRoadmap): add fallback values to roadmap tasks

--- lib/roadmap.ts
+++ lib/roadmap.ts
@@ -1,4 +1,4 @@
-import { toCamelCase } from '@/util/StringUtils';
+import { toCamelCase, transformName } from '@/util/StringUtils';
 import Papa, { ParseResult } from 'papaparse';
 
 type Roadmap = {
@@ -11,16 +11,22 @@ type Roadmap = {
     notes: string
 }
 
-async function getRoadmap():Promise<[Roadmap]> {
+async function getRoadmap():Promise<Roadmap[]> {
     const raw = await fetch(`https://docs.google.com/spreadsheets/d/1XMSkLs-BX2Il0D41sgrrOOo1J9Lje_Anj-4XJt0AZOo/export?format=csv`).then(res => res.text());
     
-    const parser = Papa.parse(raw, {
+    const { data } = Papa.parse<Roadmap>(raw, {
         header: true,
         transformHeader: header => toCamelCase(header)
     });
 
-    const data = parser.data as [Roadmap];
-    return data;
+    const transformed = data.map(task => {
+        if(transformName(task.type).length <= 0) { task.type = "Other" }
+        if(transformName(task.taskSize).length <= 0) { task.taskSize = "Undefined" }
+        if(transformName(task.status).length <= 0) { task.status = "Undefined" }
+        return task;
+    });
+
+    return transformed;
 }
 
 export { getRoadmap };
