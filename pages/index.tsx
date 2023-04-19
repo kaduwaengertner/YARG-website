58a82df98fea70aa8ac57d4fe979a5fe7c611576
Panto! <guustavopantoja@gmail.com>
2023-04-19 04:16:35 -0300

    feat(home): game screenshot

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
 import Footer from '@/components/Footer'
 import { FullLogo } from '@/components/Logo'
 import { CSSProperties } from 'react'
-
+import gameScreenshot from '@/public/game-screenshot.png';
 export default function Home() {
   return (
     <>
@@ -19,7 +20,9 @@ export default function Home() {
 
       <header className={styles.header}>
         <FullLogo className={styles.logo}/>
-        <div className={styles.video}></div>
+        <div className={styles.video}>
+          <Image src={gameScreenshot} alt="In-game Screenshot"/>
+        </div>
         <div className={styles.buttons}>
           <a href="https://github.com/EliteAsian123/YARG/releases/latest">
             <HeaderButton background="rgb(var(--accent))" style={{textTransform: "none"}}>DOWNLOAD v0.8.2</HeaderButton>
