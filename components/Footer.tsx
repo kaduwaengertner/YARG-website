e70f290579f5365ca7408394033477eb3c3f2463
Panto! <guustavopantoja@gmail.com>
2023-04-23 05:49:22 -0300

    feat: add labels to footer links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -11,9 +11,9 @@ const Footer: React.FC = () => {
 
     return (<footer className={styles.footer}>
         <div className={styles.socials}>
-            <a className={styles.twitter} href="https://twitter.com/EliteAsian123"><FontAwesomeIcon icon={faTwitter} /></a>
-            <a className={styles.discord} href="https://discord.gg/sqpu4R552r"><FontAwesomeIcon icon={faDiscord} /></a>
-            <a className={styles.github} href="https://github.com/EliteAsian123/YARG"><FontAwesomeIcon icon={faGithub} /></a>
+            <a className={styles.twitter} href="https://twitter.com/EliteAsian123" aria-label="EliteAsian123's Twitter"><FontAwesomeIcon icon={faTwitter} /></a>
+            <a className={styles.discord} href="https://discord.gg/sqpu4R552r" aria-label="YARG Discord Group"><FontAwesomeIcon icon={faDiscord} /></a>
+            <a className={styles.github} href="https://github.com/EliteAsian123/YARG" aria-label="YARG Github Repository"><FontAwesomeIcon icon={faGithub} /></a>
         </div>
         <div className={styles.text}>
             Made with a lot of <FontAwesomeIcon icon={faHeart} color="#FF9494" /> by <span className={styles.contributorsCount}>{contributors?.length || ""}</span> amazing people
