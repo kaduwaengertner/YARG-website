9543ab9b89abb1af515a24fdb588cff1d7cc8f01
Panto! <guustavopantoja@gmail.com>
2023-05-10 12:42:59 -0300

    refactor: tag button on filtergrid

---
+++ app/roadmap/filters/FilterGrid/TagButton.tsx
@@ -0,0 +1,93 @@
+import { transformName } from "@/util/StringUtils";
+import { useContext } from "react";
+import { FilterDispatchContext, FiltersContext } from "../provider";
+import Tag from "@/app/components/Tags";
+
+type filterTypes = 'category' | 'priority' | 'status';
+
+type Props = {
+    name: string,
+    type: filterTypes,
+}
+
+const TagButton: React.FC<Props> = ({name, type}) => {
+
+    const filters = useContext(FiltersContext);
+    const dispatch = useContext(FilterDispatchContext);
+
+    const filtersByType: {[key in typeof type]: string[]} = {
+        'category': filters.categoryFilter,
+        'priority': filters.priorityFilter,
+        'status': filters.statusFilter,
+    }
+
+    const togglerByType: {[key in typeof type]: Function} = {
+        'category': dispatch.toggleCategory,
+        'priority': dispatch.togglePriority,
+        'status': dispatch.toggleStatus,
+    }
+
+    const colorsByType: {[key in typeof type]: Function} = {
+        'category': getDefaultColor,
+        'priority': getDefaultColor,
+        'status': getStatusColor,
+    }
+
+    return (
+        <Tag 
+            key={name} 
+            onAction={() => { togglerByType[type](name) }} 
+            attributes={{ "data-active": !filtersByType[type].includes(transformName(name)) }} 
+            className="clickable"
+            background={colorsByType[type](name).background}
+            color={colorsByType[type](name).color}
+        >
+            <span>{name}</span>
+        </Tag>
+
+    );
+}
+
+export default TagButton;
+
+
+type TagColor = {
+    background: string,
+    color: string
+}
+
+function getDefaultColor(): TagColor {
+    return {
+        background: "var(--tag-background)",
+        color: "rgb(var(--accent))"
+    }}
+
+function getStatusColor(status: string): TagColor {
+
+    const lowercase = status.toLowerCase();
+
+    if(lowercase.includes('done')) return {
+        background: "var(--green)",
+        color: "var(--background)"
+    }
+
+    if(lowercase.includes('todo')) return {
+        background: "var(--red)",
+        color: "var(--background)"
+    }
+
+    if(lowercase.includes('stalled')) return {
+        background: "var(--blue)",
+        color: "var(--background)"
+    }
+
+    if(lowercase.includes('doing')) return {
+        background: "var(--yellow)",
+        color: "var(--background)"
+    }
+
+    return {
+        background: "var(--tag-background)",
+        color: "rgb(var(--accent))"
+    }
+}
--- app/roadmap/filters/FilterGrid/index.tsx
+++ app/roadmap/filters/FilterGrid/index.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import { useContext, useEffect } from 'react';
+import { useContext } from 'react';
 import styles from './FilterGrid.module.css';
 import { FilterDispatchContext, FiltersContext } from '../provider';
-import { transformName } from '@/util/StringUtils';
 import Tag from '@/app/components/Tags';
+import TagButton from './TagButton';
 
 type Props = {
     categoryList: string[],
@@ -44,13 +44,8 @@ const FilterGrid: React.FC<Props> = ({categoryList, priorityList, statusList}) =
                         </div>
                     </div>
 
-                    {categoryList.map(category => {
-                        return (
-                            <Tag key={category} onAction={() => { toggleCategory(category) }} attributes={{ "data-active": categoryFilter.includes(transformName(category)) }} className="clickable">
-                                <span>{category}</span>
-                            </Tag>
-                        )
-                    })}
+                    {categoryList.map(category => <TagButton key={category}  name={category} type="category" />)}
+                    
                 </div>
 
                 <div>
@@ -62,14 +57,8 @@ const FilterGrid: React.FC<Props> = ({categoryList, priorityList, statusList}) =
                         </div>
                     </div>
 
-                    {priorityList.map(priority => {
-                        const color = getPriorityColor(priority);
-                        return (
-                            <Tag key={priority} onAction={() => { togglePriority(priority) }} attributes={{ "data-active": priorityFilter.includes(transformName(priority)) }} className="clickable" background={color.background} color={color.color}>
-                                <span>{priority}</span>
-                            </Tag>
-                        )
-                    })}
+                    {priorityList.map(priority => <TagButton key={priority}  name={priority} type="priority" />)}
+
                 </div>
 
                 <div>
@@ -81,14 +70,8 @@ const FilterGrid: React.FC<Props> = ({categoryList, priorityList, statusList}) =
                         </div>
                     </div>
 
-                    {statusList.map(status => {
-                        const color = getStatusColor(status);
-                        return (
-                            <Tag key={status} onAction={() => { toggleStatus(status) }} attributes={{ "data-active": statusFilter.includes(transformName(status)) }} className="clickable" background={color.background} color={color.color}>
-                                <span>{status}</span>
-                            </Tag>
-                        );
-                    })}
+                    {statusList.map(status => <TagButton key={status}  name={status} type="status" />)}
+
                 </div>
             </div>
         ) : <></>
@@ -97,45 +80,3 @@ const FilterGrid: React.FC<Props> = ({categoryList, priorityList, statusList}) =
 }
 
 export default FilterGrid;
-
-type TagColor = {
-    background: string,
-    color: string
-}
-
-function getPriorityColor(taskSize: string): TagColor {
-    return {
-        background: "var(--tag-background)",
-        color: "rgb(var(--accent))"
-    }
-}
-
-function getStatusColor(status: string): TagColor {
-
-    const lowercase = status.toLowerCase();
-
-    if(lowercase.includes('done')) return {
-        background: "var(--green)",
-        color: "var(--background)"
-    }
-
-    if(lowercase.includes('todo')) return {
-        background: "var(--red)",
-        color: "var(--background)"
-    }
-
-    if(lowercase.includes('stalled')) return {
-        background: "var(--blue)",
-        color: "var(--background)"
-    }
-
-    if(lowercase.includes('doing')) return {
-        background: "var(--yellow)",
-        color: "var(--background)"
-    }
-
-    return {
-        background: "var(--tag-background)",
-        color: "rgb(var(--accent))"
-    }
-}
