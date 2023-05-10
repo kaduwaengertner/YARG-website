64697b1119a4723f3e965ef05abe42af93346886
Panto! <guustavopantoja@gmail.com>
2023-05-10 19:43:32 -0300

    feat: redirects

--- next.config.js
+++ next.config.js
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/download',
+                destination: 'https://github.com/YARC-Official/YARG/releases/latest',
+                permanent: true,
+            },
+
+            {
+                source: '/download/latest',
+                destination: 'https://github.com/YARC-Official/YARG/releases/latest',
+                permanent: true,
+            },
+
+            {
+                source: '/download/dev',
+                destination: 'https://drive.google.com/drive/folders/1WAxRqtQ-EF1ryF99sH8GJSS8SjMJj06u?usp=share_link',
+                permanent: true,
+            },
+
+        ];
+    },
+}
 
 module.exports = nextConfig
