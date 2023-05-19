3e3f6debbdfa3d7ac2c16c0ebff95d4a8bfa1300
Panto! <guustavopantoja@gmail.com>
2023-05-19 00:33:46 -0300

    feat(routes): move to youtrack as source

---
+++ lib/youtrack.ts
@@ -0,0 +1,47 @@
+const baseURL = "https://yarg.youtrack.cloud/api";
+
+async function api(path: string) {
+    const raw = await fetch(
+        `${baseURL}/${path}`,
+        {
+            next: {
+                revalidate: 1800,
+                tags: ["youtrack"]
+            }
+        }).then(res => res.json());
+
+    return raw;
+};
+
+type Issue = {
+    task: string,
+    description: string,
+    category: string,
+    priority: string,
+    status: string,
+    version: string
+}
+
+async function getIssues(): Promise<Issue[]> {
+
+    const raw = await api("issues?fields=summary,description,resolved,customFields(value(name),projectCustomField(field(name)))");
+
+    const issues = raw.map((issue: any) => ({
+        task: issue.summary,
+        description: issue.description,
+        category: getCustomField(issue, "Type")?.value?.name || "",
+        priority: getCustomField(issue, "Priority")?.value?.name || "",
+        status: getCustomField(issue, "State")?.value?.name || "",
+        version: getCustomField(issue, "Version")?.value?.name || "",
+    }));
+    
+    return issues;
+};
+
+function getCustomField(issue: any, customFieldName: string) {
+    const customField = issue?.customFields?.find((customField: any) => customField?.projectCustomField?.field?.name?.toLowerCase() === customFieldName.toLowerCase());
+    console.log(customField);
+    return customField
+};
+
+export { getIssues };
