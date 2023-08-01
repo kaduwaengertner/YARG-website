4b167a058d2d6e72a43c9c03b541bbddc3c85619
Panto! <guustavopantoja@gmail.com>
2023-08-01 14:10:35 -0300

    feat: new meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,31 +3,35 @@ import { Barlow } from 'next/font/google'
 
 const barlow = Barlow({ weight: ['400', '500', '600', '700'], subsets: ["latin"] })
 
+const defaultTitle = "YARG | Yet Another Rhythm Game";
+const description = "YARG (a.k.a. Yet Another Rhythm Game) is a free, open-source, plastic guitar game that is still in development. It supports guitar (five fret), drums (plastic or e-kit), vocals, pro-guitar, and more!";
+const twitterAccount = "@YARGGame";
+
 export const metadata = {
   title: {
     template: '%s | Yet Another Rhythm Game',
   },
 
-  description: 'Yet Another Rhythm Game inspired off of Rockband, Guitar Hero, Clone Hero, or similar.',
+  description: description,
   keywords: ["YARG", "Yet Another Rhythm Game", "Rock Band", "Guitar Hero", "Clone Hero"],
-  creator: "@EliteAsian123",
+  creator: twitterAccount,
   themeColor: "#080b13",
 
   openGraph: {
     locale: "en-US",
     type: "website",
-    title: "YARG | Yet Another Rhythm Game",
-    siteName: "YARG | Yet Another Rhythm Game",
-    description: "Yet Another Rhythm Game inspired off of Rockband, Guitar Hero, Clone Hero, or similar.",
+    title: defaultTitle,
+    siteName: defaultTitle,
+    description: description,
     url: "https://yarg.in",
   },
 
   twitter: {
     card: "summary_large_image",
-    title: "YARG | Yet Another Rhythm Game", 
-    description: "Yet Another Rhythm Game inspired off of Rockband, Guitar Hero, Clone Hero, or similar.",
+    title: defaultTitle, 
+    description: description,
     site: "https://yarg.in",
-    creator: "@EliteAsian123",
+    creator: twitterAccount,
     image: "https://yarg.in/banner.png",
   }
 }
