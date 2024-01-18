8f4036d432dc23a9993b03e0085fe263a4b11833
Panto! <guustavopantoja@gmail.com>
2024-01-18 17:16:18 -0300

    chore: update npm modules

--- hooks/useSWR.ts
+++ hooks/useSWR.ts
@@ -3,6 +3,6 @@ import useSWR from "swr";
 export default function SWR<T>(url: string) {
     return useSWR<T>(
             url, 
-            url => fetch(url).then(res => res.json())
+            (url: string) => fetch(url).then(res => res.json())
         );
 };
